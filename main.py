bc6bac651ec54551ef37508e8392f8b1da1ecb4c
Suyash Sakhare <suyash.sakhare007@ku.edu>
2023-03-23 19:36:59 -0500

    Adding Changes

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import numpy as np
-#from Random import Random
-
-def coin_flips(p, N):
-    """ Simulate N coin flips with a biased coin with probability of heads p """
-    flips = np.random.binomial(1, p, N)  # 1 represents heads, 0 represents tails
-    return np.sum(flips)  # return the number of heads
-
-
+from Random import Random
 import matplotlib.pyplot as plt
+
+#Changing parameters
+seed = 5555
+N_Coin = 10 # Number of Coins/Measurements
+Nexp = 1000 # Number of Experiments
+
+
 
 true_p_values = np.linspace(0,1, 101)  # 101 evenly spaced true values of p from 0 to 1
 measured_p_values = [] # initialize an array to store the measured values
-true = []
+true = [] #Store the repeated true values
+
+Coins = Random(seed)
+
+
 for i in true_p_values:
-    for j in range(1000):
-        d = coin_flips(i,10)
+    for j in range(Nexp):
+        d = Coins.Binomial(N_Coin, i)
         measured_p_values.append(d)
         true.append(i)
     
 plt.hist2d(true, measured_p_values)
 plt.xlabel("True p")
 plt.ylabel("Measured p")
-plt.show()
+plt.savefig("Construction")
